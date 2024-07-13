481b9e5464bf2fe2392eb9e38dd25892cee78e0f
FallibleVagrant <124470389+FallibleVagrant@users.noreply.github.com>
2024-07-13 13:57:39 -0700

    [Client] Give feedback to user after sending a message.

--- client/src/main.rs
+++ client/src/main.rs
@@ -5,17 +5,20 @@ fn button(dc: &mut DrawingContext, x: i32, y: i32, w: i32, h: i32, text: &str, b
     let mouse_pos = get_mouse_position();
     let mouse_x = mouse_pos.x as i32;
     let mouse_y = mouse_pos.y as i32;
+    let mut is_pressed = false;
 
     if mouse_x >= x && mouse_x <= x + w
         && mouse_y >= y && mouse_y <= y + h {
 
-        dc.draw_rectangle(x, y, w, h, Color { r: 100, g: 100, b: 100, a: 255 });
-        dc.draw_rectangle(x + 2, y + 2, w - 4, h - 4, bg_color);
-
         if is_mouse_button_pressed(MouseButton::Left) {
-            dc.draw_text(text, x, y, 20, colors::WHITE);
-            return true;
+            dc.draw_rectangle(x, y, w, h, Color { r: 200, g: 200, b: 200, a: 255 });
+            is_pressed = true;
         }
+        else {
+            dc.draw_rectangle(x, y, w, h, Color { r: 100, g: 100, b: 100, a: 255 });
+        }
+
+        dc.draw_rectangle(x + 2, y + 2, w - 4, h - 4, bg_color);
     }
     else {
         dc.draw_rectangle(x, y, w, h, bg_color);
@@ -25,7 +28,16 @@ fn button(dc: &mut DrawingContext, x: i32, y: i32, w: i32, h: i32, text: &str, b
     let ascii_size = measure_text_ex(get_default_font(), text, font_size as f32, 1.5);
     dc.draw_text(text, x + w/2 - (ascii_size.x / 2.0) as i32, y + h/2 - (ascii_size.y / 2.0) as i32, 20, colors::WHITE);
 
-    return false;
+    return is_pressed;
+}
+
+use std::time::{Duration, Instant};
+use std::thread;
+
+fn sleep_until(time: Instant) {
+    if time > Instant::now() {
+        thread::sleep(time - Instant::now());
+    }
 }
 
 fn main() {
@@ -41,7 +53,15 @@ fn main() {
     let mut msg = String::new();
     let mut err_msg = String::new();
 
+    let max_fps = 30.0;
+    let frame_time = Duration::from_secs_f32(1.0/max_fps);
+    let mut next_frame = Instant::now();
+
     while !wc.window_should_close() {
+        //Sleep until next frame.
+        sleep_until(next_frame);
+        next_frame += frame_time;
+
         let mut dc = wc.init_drawing_context();
         dc.clear_background(Color { r: 25, g: 75, b: 75, a: 255 });
 
@@ -82,10 +102,17 @@ fn main() {
         dc.draw_text(txt, x, y - 90, font_size, colors::WHITE);
 
         //Draw the error message.
+        let color;
+        if err_msg.starts_with("ERR:") {
+            color = colors::RED;
+        }
+        else {
+            color = colors::GREEN;
+        }
         let ascii_size = measure_text_ex(get_default_font(), &err_msg, font_size as f32, 1.5);
         let x = middle_width - (ascii_size.x / 2.0) as i32;
         let y = middle_height - (ascii_size.y / 2.0) as i32;
-        dc.draw_text(&err_msg, x, y - 120, font_size, colors::RED);
+        dc.draw_text(&err_msg, x, y - 120, font_size, color);
 
         //Now draw the buttons:
 
@@ -100,6 +127,7 @@ fn main() {
             }
             else {
                 session.send_info(&msg);
+                err_msg = "Sent!".to_string();
             }
         }
 
@@ -110,6 +138,7 @@ fn main() {
         let y = middle_height - (h / 2) + offset;
         if button(&mut dc, x, y, w, h, "WARN", Color { r: 244, g: 131, b: 37, a: 255 }) {
             session.send_warn(&msg);
+            err_msg = "Sent!".to_string();
         }
 
         let w = 150;
@@ -119,6 +148,7 @@ fn main() {
         let y = middle_height - (h / 2) + offset;
         if button(&mut dc, x, y, w, h, "ALERT", Color { r: 179, g: 0, b: 0, a: 255 }) {
             session.send_alert(&msg);
+            err_msg = "Sent!".to_string();
         }
     }
 }
